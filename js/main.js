7c5099a6aafc44c5cd66e23cb479e724d6335b48
evahuertas <evahuegar@gmail.com>
2024-05-15 15:52:02 +0200

    fix: funcionalidad básica completada

--- js/main.js
+++ js/main.js
@@ -38,15 +38,15 @@ function differenceBetweenCurrentDateAndInputDate (inputDay, inputMonth, inputYe
     console.log( 'inputDate :' + inputDate)
 
     // fecha de entrada en formato timestamp
-    var inputDateFormat = new Date(inputDate)
+    var inputDateFormat = new Date(inputDate);
     console.log( 'inputDateFormat :' + inputDateFormat)
 
     // fecha de entrada en Milisegundos
-    var inputDateFormatMS = new Date(inputDate).getTime()
+    var inputDateFormatMS = new Date(inputDate).getTime();
     console.log( 'inputDateFormatMS :' + inputDateFormatMS)
 
     // comparar fechas en milisegundos
-    var differenceBetweenDatesMS = currentDateMS - inputDateFormatMS 
+    var differenceBetweenDatesMS = currentDateMS - inputDateFormatMS ;
     console.log('differenceBetweenDatesMS :' + differenceBetweenDatesMS)
 
     formatDifferenceBetweenDates (differenceBetweenDatesMS)
@@ -57,22 +57,36 @@ function formatDifferenceBetweenDates (differenceBetweenDatesMS) {
     // console.log('new log differenceBetweenDatesMS :' + differenceBetweenDatesMS)
 
     //obtener la cantidad de dias del total de milisegundos
-    var days = Math.floor(differenceBetweenDatesMS / 1000 / 60 / 60 / 24)
-    console.log('days :' + days)
+    var totalDays = Math.floor(differenceBetweenDatesMS / 1000 / 60 / 60 / 24);
+    console.log('totalDays :' + totalDays)
 
     //obtener la cantidad de años del total de días
-    var years = Math.floor(days / 365)
+    var years = Math.floor(totalDays / 365);
     console.log('years :' + years)
 
-    addToHtml(years)
+    //obtener la cantidad de meses del total de meses en días
+    var totalMonths = Math.floor(totalDays / 30.5);
+    console.log('totalMonths :' + totalMonths)
+    var months = totalMonths - years * 12
+
+    //obtener la cantidad de días restantes
+    var days = totalDays - months * 30.5 - ((years * 12 )* 30.5);
+
+
+    addToHtml(years, months, days);
 };
 
-function addToHtml (years){
+function addToHtml (years, months, days){
 
-    // console.log(' new log years :' + years)
+    console.log(' new log years :' + years + months)
 
     document.querySelector('#output-years').textContent = years
-    console.log('inputYear :' + inputYear);
+    console.log('outputYear :' + years);
+    
+    document.querySelector('#output-months').textContent = months
+    console.log('outputMonth :' + months);
 
+    document.querySelector('#output-days').textContent = days
+    console.log('outputDay :' + days);
 
 };
